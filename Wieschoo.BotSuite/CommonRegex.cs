a7ed5150249986bf75e255e93cc6c0efd03f1ece
gigagames <gigagames@609c14f8-e7f3-49a0-8c3a-bfaf6b541b75>
2013-03-01 15:24:49 +0000

    RegExp Search überarbeitet Optionen Variable eingefügt
    - Bitte wer einen Text für Summary hinzufügen da mein eng. schlecht ist 
    
    git-svn-id: http://svn.wieschoo.info/svn/BotSuite@16 609c14f8-e7f3-49a0-8c3a-bfaf6b541b75

--- Wieschoo.BotSuite/CommonRegex.cs
+++ Wieschoo.BotSuite/CommonRegex.cs
@@ -10,11 +10,12 @@ namespace BotSuite
         /// </summary>
         /// <param name="Source">text where looking for</param>
         /// <param name="Pattern">pattern to look for</param>
+        /// <param name="Optionen">Optionen</param>
         /// <returns>array of result</returns>
-        public static String[,] Search(String Pattern,String Source)
+        public static String[,] Search(this String Source, String Pattern, RegexOptions Optionen = RegexOptions.None)
         {
-            if (!Regex.IsMatch(Source, Pattern)) return null;
-            MatchCollection aMatch = Regex.Matches(Source, Pattern, RegexOptions.None);
+            if (!Regex.IsMatch(Source, Pattern, Optionen)) return null;
+            MatchCollection aMatch = Regex.Matches(Source, Pattern, Optionen);
             String[,] RetString = new String[aMatch.Count, aMatch[0].Groups.Count - 1];
             Int32 i = 0;
             Int32 ii = 0;
@@ -34,29 +35,6 @@ namespace BotSuite
             return RetString;
         }
 
-
-
-
-
-        /*
-        public static String[,] Regexp(this String Source, String Pattern)
-        {
-            if (!Regex.IsMatch(Source, Pattern)) return null;
-            MatchCollection aMatch = Regex.Matches(Source, Pattern, RegexOptions.None);
-            String[,] RetString = new String[aMatch.Count - 1, aMatch[0].Groups.Count - 1];
-            for (Int32 i = 0; i <= aMatch.Count - 1; i++)
-            {
-                //RetString[i, 0] = aMatch[i].Captures[0].Value;
-                for (Int32 ii = 0; ii <= aMatch[i].Groups.Count - 2; ii++)
-                {
-                    RetString[i, ii] = aMatch[ii].Groups[0].Value;
-                }
-            }
-            return RetString;
-        }
-        */
-
-
         /// <summary>
         /// returns a string between two strings
         /// </summary>
@@ -64,7 +42,7 @@ namespace BotSuite
         /// <param name="Start">before the string we are looking for</param>
         /// <param name="End">after the string we are looking for</param>
         /// <returns>all results</returns>
-        public static String[] Between(String Start, String End, String Source)
+        public static String[] Between(this String Source, String Start, String End)
         {
             string Pattern = Start + "(.*)?" + End;
             String[] tString;
