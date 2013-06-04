f7a092de54c9e27916c27d92f8299c0ce04007b2
Roman Esseln <wewantthejoker@gmail.com>
2013-06-04 23:15:18 +0200

    Added Pattern Class ( not yet implemented )

---
+++ Wieschoo.BotSuite/Pattern.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BotSuite
+{
+    class Pattern
+    {
+        // Will be filled by Gr3enP0ison
+    }
+}
