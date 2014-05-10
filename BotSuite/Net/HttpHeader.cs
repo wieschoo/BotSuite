e21508d7494cb35b77b25d3399847fd22cccc61f
Derpy Hooves <endusa.works@gmail.com>
2014-05-10 19:12:54 +0200

    First Commit of Changes regarding /Net classes. More changes to come, but require more work since i completely changed the structure of the project

--- BotSuite/Net/HttpHeader.cs
+++ BotSuite/Net/HttpHeader.cs
@@ -1,68 +1,80 @@
-﻿//-----------------------------------------------------------------------
-// <copyright file="HttpHeader.cs" company="Wieschoo &amp; enWare">
-//     Copyright (c) Wieschoo &amp; enWare.
-// </copyright>
-// <project>BotSuite.Net</project>
-// <purpose>framework for creating bots</purpose>
-// <homepage>http://botsuite.net/</homepage>
-// <license>http://botsuite.net/license/index/</license>
-//-----------------------------------------------------------------------
+﻿// -----------------------------------------------------------------------
+//  <copyright file="HttpHeader.cs" company="Wieschoo &amp; Binary Overdrive">
+//      Copyright (c) Wieschoo &amp; Binary Overdrive.
+//  </copyright>
+//  <project>BotSuite.Net</project>
+//  <purpose>framework for creating bots</purpose>
+//  <homepage>http://botsuite.net/</homepage>
+//  <license>http://botsuite.net/license/index/</license>
+// -----------------------------------------------------------------------
 
 namespace BotSuite.Net
 {
 	using System;
 
 	/// <summary>
-	/// represents a HTTP header field
+	///     represents a HTTP header field
 	/// </summary>
 	public class HttpHeader : IEquatable<HttpHeader>, IComparable<HttpHeader>
 	{
 		/// <summary>
-		/// Gets the name/key of the header field
+		///     Gets the name/key of the header field
 		/// </summary>
-		public String Key { get; private set; }
+		public string Key { get; private set; }
 
 		/// <summary>
-		/// Gets the value of the header field
+		///     Gets the value of the header field
 		/// </summary>
-		public String Value { get; private set; }
+		public string Value { get; private set; }
 
 		/// <summary>
-		/// Initializes a new instance of the <see cref="HttpHeader"/> class
+		///     Initializes a new instance of the <see cref="HttpHeader" /> class
 		/// </summary>
-		/// <param name="key">the name/key of the header field</param>
-		/// <param name="value">the value of the header field</param>
-		public HttpHeader(String key, String value)
+		/// <param name="key">
+		///     the name/key of the header field
+		/// </param>
+		/// <param name="value">
+		///     the value of the header field
+		/// </param>
+		public HttpHeader(string key, string value)
 		{
 			this.Key = key;
 			this.Value = value;
 		}
 
 		/// <summary>
-		/// compares 2 instances of the HttpHeader class for equality
+		///     compares 2 instances of the HttpHeader class for equality
 		/// </summary>
-		/// <param name="other">the instance to compare to</param>
-		/// <returns>true, if both are equal, else false</returns>
-		public Boolean Equals(HttpHeader other)
+		/// <param name="other">
+		///     the instance to compare to
+		/// </param>
+		/// <returns>
+		///     true, if both are equal, else false
+		/// </returns>
+		public bool Equals(HttpHeader other)
 		{
 			return this.Key.Equals(other.Key);
 		}
 
 		/// <summary>
-		/// compares the order of 2 instances of the HttpClass
+		///     compares the order of 2 instances of the HttpClass
 		/// </summary>
-		/// <param name="other">the instance to compare to</param>
-		/// <returns>an integer determing the order of both elements</returns>
-		public Int32 CompareTo(HttpHeader other)
+		/// <param name="other">
+		///     the instance to compare to
+		/// </param>
+		/// <returns>
+		///     an integer determing the order of both elements
+		/// </returns>
+		public int CompareTo(HttpHeader other)
 		{
-			return this.Key.CompareTo(other.Key);
+			return string.Compare(this.Key, other.Key, StringComparison.Ordinal);
 		}
 
 		/// <summary>
-		/// creates a string representation of the current instance of this class
+		///     creates a string representation of the current instance of this class
 		/// </summary>
 		/// <returns>a string representation of the current instance of this class</returns>
-		public override String ToString()
+		public override string ToString()
 		{
 			return "HttpHeader->{" + this.Key + ": " + this.Value + "}";
 		}
